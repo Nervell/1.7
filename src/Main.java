4d715d868348e2199af2a05fbe0145c1506ca14f
Bogdan Volobuev <9803459870b@gmail.com>
2022-05-31 15:26:43 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание номер 1
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        //Задание номер 2
+        String upperFullName = fullName.toUpperCase();
+        System.out.println(upperFullName);
+
+        //Задание номер 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
